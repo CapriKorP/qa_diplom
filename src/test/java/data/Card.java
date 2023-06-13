c92c9af47f5c96b3ef252af68adcec25c8cbd219
caprikorp <pikornienkov@gmail.com>
2023-06-13 09:42:14 +0300

    Add Card.java and TestDataGenerator.java

--- src/test/java/data/Card.java
+++ src/test/java/data/Card.java
@@ -1,9 +1,18 @@
 package data;
 
 public class Card {
+
     public String number;
     public String month;
     public String year;
-    public String Holder;
+    public String holder;
     public String cvc;
+
+    public Card(String number, String month, String year, String holder, String cvc) {
+        this.number = number;
+        this.month = month;
+        this.year = year;
+        this.holder = holder;
+        this.cvc = cvc;
+    }
 }
